3bb758fec5b7da6df9e1de17f47e3ea0eb30432c
serif <tgtsserif@gmail.com>
2022-03-24 07:59:01 +0700

    temporary issue

--- js/bigshot.js
+++
@@ -1,6 +0,0 @@
-function hyperlink() {
-  alert("HYPERLINK [BLOCKED!!!]");
-}
-
-document.getElementById("hyperlink").addEventListener("click", hyperlink);
-document.getElementById("hyperlink2").addEventListener("click", hyperlink);
