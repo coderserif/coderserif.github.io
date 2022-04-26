c8a770e233b3e182dfdcebccf5dc9426af3bd129
serif <tgtsserif@gmail.com>
2022-04-26 11:04:45 +0700

    TIMER REVAMP
    i made it better

--- js/timer.js
+++ js/timer.js
@@ -1,43 +1,53 @@
-var myVar;
-var timer = document.getElementById("userInput");
-var countDownSeconds;
-function startTime(){ 
-  myVar = setInterval(start, 1000);
-  document.getElementById("timerr").innerHTML = timer.value + "s";
-  countDownSeconds = timer.value;
-} 
+var start = document.getElementById('start');
+var reset = document.getElementById('reset');
 
-function start(){
-  countDownSeconds--;
-  document.getElementById("timerr").innerHTML = countDownSeconds + "s";
-  if (countDownSeconds == -1){
-    stop();
-    document.getElementById("timerr").innerHTML = "FINISHED :D";
-    var audio = new Audio('audio/wakeup.mp3');
-    audio.play();
-  }
+var h = document.getElementById("hour");
+var m = document.getElementById("minute");
+var s = document.getElementById("sec");
+
+//store a reference to the startTimer variable
+var startTimer = null;
+
+start.addEventListener('click', function(){
+    //initialize the variable
+    function startInterval(){
+        startTimer = setInterval(function() {
+            timer();
+        }, 1000);
+    }
+    startInterval();
+})
+
+reset.addEventListener('click', function(){
+    h.value = 0;
+    m.value = 0;
+    s.value = 0;
+    //stop the timer after pressing "reset"
+    stopInterval()
+})
+
+function timer(){
+    if(h.value == 0 && m.value == 0 && s.value == 0){
+        h.value = 0;
+        m.value = 0;
+        s.value = 0;
+        var audio = new Audio('audio/wakeup.mp3');
+        audio.play();
+    } else if(s.value != 0){
+        s.value--;
+    } else if(m.value != 0 && s.value == 0){
+        s.value = 59;
+        m.value--;
+    } else if(h.value != 0 && m.value == 0){
+        m.value = 60;
+        h.value--;
+    }
+    return;
 }
 
-function stop(){
-  clearInterval(myVar);
+//stop the function after pressing the reset button, 
+//so the time wont go down when selecting a new time after pressing reset
+function stopInterval() {
+    clearInterval(startTimer);
 }
 
-function convertToSeconds() {
-    inputVal = document.getElementById("userInput1").value;
-    document.getElementById("numberOfHours").innerHTML = inputVal * 3600;
-
-}
-
-function convertToSeconds2() {
-    inputVal = document.getElementById("userInput2").value;
-    document.getElementById("numberOfMinutes").innerHTML = inputVal * 60;
-
-}
-
-function add() {
-    var num1, num2, sum;
-    num1 = parseInt(document.getElementById("userInput3").value);
-    num2 = parseInt(document.getElementById("userInput4").value);
-    sum = num1 + num2;
-    document.getElementById("totalSeconds").innerHTML = sum;
-  }
